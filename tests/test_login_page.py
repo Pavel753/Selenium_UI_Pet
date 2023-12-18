c7e08c3eecd6e471e1d428b8cdd0cf488bf28b4a
pif34 <pif345678@gmail.com>
2023-12-18 20:34:36 +0400

    L12.1

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,4 +1,5 @@
 from pages.login_page import LoginPage
+from selenium.webdriver.common.by import By
 
 
 def test_go_to_login(browser):
@@ -8,4 +9,6 @@ def test_go_to_login(browser):
     page.go_to_login()
     page.go_to_password()
     page.click_button()
+    browser.implicitly_wait(5)
+    browser.find_element(By.XPATH, '//*[@id="app"]/header/div/ul/li[1]/a/span[2]')
     browser.save_screenshot('result6.png')
